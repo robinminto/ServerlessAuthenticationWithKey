6c217cb6d7bf0a5493713bbcfff800fde284bcde
Robin Minto <robin@minto.co.uk>
2019-09-04 17:26:14 +0100

    Route experimenting

--- ServerlessAuthenticationWithKey/GreetingsEngine.cs
+++ ServerlessAuthenticationWithKey/GreetingsEngine.cs
@@ -14,7 +14,7 @@ namespace ServerlessAuthenticationWithKey
     {
         [FunctionName("Greeting")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "test")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
